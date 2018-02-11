34383efae9798ca7b3a661c0af87b6c576be8813
shikhavaidh <36124149+shikhavaidh@users.noreply.github.com>
2018-02-11 23:11:52 +0530

    Assignment 2.3.py
    Assignment 2.3.py

---
+++ Assignment 2.3.py
@@ -0,0 +1,25 @@
+
+# coding: utf-8
+
+# In[34]:
+
+val = "AcadGild"
+length = len(val)
+print (length)
+text_rev = ""
+while length>0:
+   text_rev += val[length-1]
+   length = length-1
+   print (length)
+print (text_rev)
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
