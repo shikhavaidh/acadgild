88ce29b8038a46947df81b6efd70ad16102b96a4
shikhavaidh <36124149+shikhavaidh@users.noreply.github.com>
2018-02-12 21:59:56 +0530

    Assignment 2.4
    Assignment 2.4
    Output

---
+++ Assignment 2.4.py
@@ -0,0 +1,17 @@
+
+# coding: utf-8
+
+# In[16]:
+
+input="WE, THE PEOPLE OF INDIA, having solemnly resolved to constitute India into a SOVEREIGN, SOCIALIST, SECULAR, DEMOCRATIC REPUBLIC and to secure to all its citizens"
+print("Input String - \n" + input + "\n")
+
+str = "WE, THE PEOPLE OF INDIA, \n \t having solemnly resolved to constitute India into a SOVEREIGN,"+ "! \n\t\t" + "SOCIALIST, SECULAR, DEMOCRATIC REPUBLIC \n \t \t and to secure to all its citizens"+":"
+
+print("Formatted Output: \n" + str)
+
+
+# In[ ]:
+
+
+
